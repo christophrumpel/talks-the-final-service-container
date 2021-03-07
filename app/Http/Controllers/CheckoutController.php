ce897077ccf213d1e6f21f961af90dd97811a5ce
Christoph Rumpel <c.rumpel@kabsi.at>
2021-03-07 11:18:11 +0100

    Reset to start

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,18 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use App\Payment\PaymentContract;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class CheckoutController extends Controller
 {
-    public function __invoke(Request $request, PaymentContract $paymentProvider): Response
+    public function __invoke(Request $request): Response
     {
-        $paymentProvider->charge($request->get('email'), $request->get('amount'));
 
-        $message = 'Successfully charged '.$paymentProvider->currency.$request->get('amount').' from '.$request->get('email').' via '.$paymentProvider->getName();
+        // Charge user
 
-        return response($message);
+        return response('message');
     }
 }
