d8ff0d3c0beb8f6a0d2c56c5bfccc0ee03152428
Christoph Rumpel <c.rumpel@kabsi.at>
2021-03-11 08:59:59 +0100

    Do not extend mail provider, use our own

--- app/Providers/CustomMailManagerProvider.php
+++ app/Providers/CustomMailManagerProvider.php
@@ -3,33 +3,22 @@
 namespace App\Providers;
 
 use App\Support\CustomMailManager;
-use Illuminate\Mail\MailServiceProvider;
+use Illuminate\Support\ServiceProvider;
 
-class CustomMailManagerProvider extends MailServiceProvider
+class CustomMailManagerProvider extends ServiceProvider
 {
     /**
-     * Register the Illuminate mailer instance.
-     *
-     * @return void
+     * Register my custom mailer instance.
      */
-    protected function registerIlluminateMailer()
+    public function register(): void
     {
+        // Same as Illuminate\Mail\MailServiceProvider
         $this->app->singleton('mail.manager', function($app) {
             return new CustomMailManager($app);
         });
 
-        // Copied from Illuminate\Mail\MailServiceProvider
         $this->app->bind('mailer', function ($app) {
             return $app->make('mail.manager')->mailer();
         });
-    }
-
-    /**
-     * Bootstrap services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
     }
 }
